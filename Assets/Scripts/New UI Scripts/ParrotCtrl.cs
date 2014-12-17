bea83786cf4b7045fc0ee909059b3fa033bd8ce7
aratingadevelopers@gmail.com <aratingadevelopers@gmail.com@712fa71d-62c7-3548-ab67-251bebb63946>
2014-12-17 08:51:31 +0000

    Animación lorito bola

---
+++ Assets/Scripts/New UI Scripts/ParrotCtrl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParrotCtrl : MonoBehaviour
+{
+	int waitTime;
+	Animator animator;
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+		waitTime = Random.Range(1,5);
+		StartCoroutine(DoBlink());
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	IEnumerator DoBlink()
+	{
+		yield return new WaitForSeconds(waitTime);
+		waitTime = Random.Range(3,7);
+		animator.SetTrigger("DoBlink");
+		StartCoroutine(DoBlink());
+	}
+}
