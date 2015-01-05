775dc4378df3cfe41aa9a499b1c94446674c2587
aratingadevelopers@gmail.com <aratingadevelopers@gmail.com@712fa71d-62c7-3548-ab67-251bebb63946>
2015-01-05 15:16:14 +0000

    Movemos archivos para ordenarlos

---
+++ trunk/Assets/Scripts/New UI Scripts/BackgroundImage.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BackgroundImage : MonoBehaviour
+{
+	public Sprite[] imgWorlds;
+	
+	void Start()
+	{
+		GetComponent<Image>().sprite = imgWorlds[LevelManager.worldNo-1];
+	}
+}
