9d4dae1b566a840b5c0ca06afcca4eb79fa06c79
aratingadevelopers@gmail.com <aratingadevelopers@gmail.com@712fa71d-62c7-3548-ab67-251bebb63946>
2015-01-20 12:59:54 +0000

    En lugar de instanciar los scores cada vez, se crean 20 instancias al principio y se van usando. Para reducir el lag del OnCollisionEnter

--- Assets/Scripts/Game Scripts/ScoreManagerGame.cs
+++ Assets/Scripts/Game Scripts/ScoreManagerGame.cs
@@ -5,9 +5,12 @@ public class ScoreManagerGame : MonoBehaviour
 {
     public static ScoreManagerGame instance;
     public GameObject scoreItemPrefab;
-    int bonusPoint;
     int numberOfItemPoppedInARow = 0;
 	GameObject DummyScore;
+	
+	GameObject[] scoreArray;
+	ScorePopupItem[] ScorePopupItemArray;
+	int maxItems=20;
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -15,21 +18,37 @@ public class ScoreManagerGame : MonoBehaviour
     {
         instance = this;
 		DummyScore = GameObject.Find("DummyScore");
+		CreateArray();
+	}
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	void CreateArray()
+	{
+		scoreArray = new GameObject[maxItems];
+		ScorePopupItemArray = new ScorePopupItem[maxItems];
+		
+		for(int i=0;i<maxItems;i++){
+			GameObject scoreItem = (GameObject)Instantiate(scoreItemPrefab, new Vector3(10000, 10000, 1), Quaternion.identity);
+			scoreItem.transform.SetParent(DummyScore.transform);
+			scoreItem.transform.localScale=new Vector3(1,1,1);
+			scoreArray[i] = scoreItem;
+			ScorePopupItemArray[i] = scoreItem.GetComponent<ScorePopupItem>();
+		}
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    internal void DisplayScorePopup(int score,Transform go)
+    internal void DisplayScorePopup(int score, Transform go)
     { 
-        GameObject scoreItem = (GameObject)Instantiate(scoreItemPrefab, go.position + new Vector3(0, 0, 1), Quaternion.identity);
-        //scoreItem.transform.eulerAngles = new Vector3(0, 180, 0);
-		scoreItem.transform.SetParent(DummyScore.transform);
-		scoreItem.transform.localScale=new Vector3(1,1,1);
-
-        bonusPoint = score * numberOfItemPoppedInARow;
-        int points = score + bonusPoint;
-
-        scoreItem.GetComponent<ScorePopupItem>().BringItForward(points);
+		if(numberOfItemPoppedInARow>maxItems){
+			return;
+		}
+		int points = score + (score * numberOfItemPoppedInARow);
+		scoreArray[numberOfItemPoppedInARow].transform.localScale = new Vector3(1, 1, 1);
+		scoreArray[numberOfItemPoppedInARow].transform.position = go.position + new Vector3(0, 0, 1);
+		ScorePopupItemArray[numberOfItemPoppedInARow].BringItForward(points);
+        	
         LevelManager.instance.AddToScore(points);
         numberOfItemPoppedInARow++;
 
@@ -45,3 +64,6 @@ public class ScoreManagerGame : MonoBehaviour
         ScorePopupItem.ResetDelay();
     }
 }
+
+
+
--- Assets/Scripts/My Utilities/ScorePopupItem.cs
+++ Assets/Scripts/My Utilities/ScorePopupItem.cs
@@ -4,27 +4,14 @@ using System.Collections;
 
 public class ScorePopupItem : MonoBehaviour 
 {
-    //public TextMesh myTextMesh;
     public Text text;
     static float delay = 0;
-
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	void Awake()
-	{
-		//renderer.sortingLayerName = "FrontLayer";
-		//transform.localScale = new Vector3(1,1,1);
-		//iTween.ScaleTo(gameObject, new Vector3(0.5f, 0.5f, 1), 0.3f);
-		//text.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-	}
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	void Start() 
     {        
         delay = .1f;
-        //renderer.material.color = Color.red;
-        //renderer.material.color = new Color(renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, .9f);
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -38,10 +25,8 @@ public class ScorePopupItem : MonoBehaviour
 
     internal void BringItForward(int score)
     {        
-        //myTextMesh.text = score.ToString();
 		text.text = score.ToString();
         Invoke("ZoomIn", delay);
-		//Invoke("ZoomIn", 0.1f);
         delay += .1f;
     }
 
@@ -58,7 +43,8 @@ public class ScorePopupItem : MonoBehaviour
     void ZoomOut()
     {
         iTween.ScaleTo(gameObject, new Vector3(0, 0, 0), .1f);
-        Destroy(gameObject, .2f);
+        //Destroy(gameObject, .2f);
+        transform.position = new Vector3(10000,10000,0);
     }
 }
 
