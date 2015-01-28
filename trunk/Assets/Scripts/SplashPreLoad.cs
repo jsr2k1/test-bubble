a3864def767dff74fc9c60bc863f2721d87696e9
aratingadevelopers@gmail.com <aratingadevelopers@gmail.com@712fa71d-62c7-3548-ab67-251bebb63946>
2015-01-28 09:50:05 +0000

    Cambios para que funcione correctamente la precarga y el mensaje de Loading

--- trunk/Assets/Scripts/SplashPreLoad.cs
+++ trunk/Assets/Scripts/SplashPreLoad.cs
@@ -1,10 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SplashPreLoad : MonoBehaviour
 {
+	public Text loadingText;
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
 	void Start()
 	{
+		StartCoroutine(LoadSplash());
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	IEnumerator LoadSplash()
+	{
+		SystemLanguage currentLang = Application.systemLanguage;
+		if(currentLang == SystemLanguage.Spanish){
+			loadingText.text="Cargando...";
+		}else{
+			loadingText.text="Loading...";
+		}
+		yield return new WaitForEndOfFrame();
 		Application.LoadLevel("02 Splash Managers");
 	}
 }
