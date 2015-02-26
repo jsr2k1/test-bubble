51947aebb82f2dee4a00c05929d1886d17d9027d
aratingadevelopers@gmail.com <aratingadevelopers@gmail.com@712fa71d-62c7-3548-ab67-251bebb63946>
2015-02-26 16:06:46 +0000

    Facebook: No se tienen en cuenta los mensajes de invitaciones  a jugar. ImgDummy: Pequeño ajuste para que el ImgDummy se coloque bien.

--- trunk/Assets/Scripts/CenterImgDummy.cs
+++ trunk/Assets/Scripts/CenterImgDummy.cs
@@ -5,8 +5,6 @@ using UnityEngine.UI;
 public class CenterImgDummy : MonoBehaviour
 {
 	public Image imgDummy;
-	float minY=1500; 	//posicion mas baja posible de la camara
-	float maxY=-1500;	//posicion mas alta posible de la camara
 	float levelMin=0;	//nivel minimo posible
 	float levelMax;		//nivel maximo posible
 	
@@ -14,23 +12,22 @@ public class CenterImgDummy : MonoBehaviour
 
 	void Start()
 	{
+		//1.- Obtenemos los valores min y max en funcion del ratio de pantalla
+		//En 16:9 --> El ImgDummy va de 1500 a -1500
+		//En 4:3 --> El ImgDummy va de 1650 a -1650
+		float ratio = Screen.height/Screen.width;
+		float t = Mathf.InverseLerp(1.777f, 1.333f, ratio);
+		float minY = Mathf.Lerp(1500, 1650, t);
+		
+		//2.- Obtenemos la posicion del ImgDummy en funcion del nivel actual
+		//En el nivel 1 tiene que estar en minY
+		//En el nivel maximo tiene que estar en -minY
 		float yPos;
 		int lvl = PlayerPrefs.GetInt("Level");
 		levelMax = LevelParser.instance.maxLevels;
 		
-		/*
-		if (lvl > -1 && lvl < 11) {
-			yPos = 788;
-		} else if (lvl > 10 && lvl < 21) {
-			yPos = 397;
-		} else if (lvl > 20 && lvl < 31) {
-			yPos = -198;
-		} else if (lvl > 30 && lvl < 41) {
-			yPos = -788;
-		}*/
-		
-		float t = Mathf.InverseLerp(levelMin, levelMax, lvl);
-		yPos = Mathf.Lerp(minY, maxY, t);
+		t = Mathf.InverseLerp(levelMin, levelMax, lvl);
+		yPos = Mathf.Lerp(minY, -minY, t);
 
 		imgDummy.transform.localPosition = new Vector3(imgDummy.transform.localPosition.x, yPos, imgDummy.transform.localPosition.z);
 	}
