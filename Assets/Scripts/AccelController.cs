98918e56e91d7b76552623ad0b84805d6b55ba0a
aratingadevelopers@gmail.com <aratingadevelopers@gmail.com@712fa71d-62c7-3548-ab67-251bebb63946>
2015-04-10 13:42:41 +0000

    El acelerometro no funciona ok con pantallas HD

--- Assets/Scripts/AccelController.cs
+++ Assets/Scripts/AccelController.cs
@@ -7,14 +7,16 @@ public class AccelController : MonoBehaviour
 	Vector3 smoothAccel;
 	float kFilteringFactor = 0.1f;
 	Vector3 originalPosition;
-	float screen_ratio;
+	float screen_ratio_h;
+	float screen_ratio_w;
 	
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
 	void Awake()
 	{
 		originalPosition = transform.localPosition;
-		screen_ratio = Screen.width / 720.0f;
+		screen_ratio_h = Screen.height / 1280.0f;
+		screen_ratio_w = Screen.width / 720.0f;
 	}
 	
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -25,6 +27,6 @@ public class AccelController : MonoBehaviour
 		smoothAccel.y = Input.acceleration.y * kFilteringFactor + smoothAccel.y * (1.0f - kFilteringFactor);
 		smoothAccel.z = Input.acceleration.z * kFilteringFactor + smoothAccel.z * (1.0f - kFilteringFactor);
 		
-		transform.localPosition = originalPosition - new Vector3(smoothAccel.x*multiplier*screen_ratio, smoothAccel.y*multiplier*screen_ratio, 0.0f);
+		transform.localPosition = originalPosition - new Vector3(smoothAccel.x*multiplier*screen_ratio_w, smoothAccel.y*multiplier*screen_ratio_h, 0.0f);
 	}
 }
