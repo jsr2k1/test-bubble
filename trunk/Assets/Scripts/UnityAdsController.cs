0756c2906e85b559cf50c6ee7924451173841dff
aratingadevelopers@gmail.com <aratingadevelopers@gmail.com@712fa71d-62c7-3548-ab67-251bebb63946>
2015-04-29 13:56:30 +0000

    Se han añadido los UnityAds v.1.4.2 (April 15th, 2015)

---
+++ trunk/Assets/Scripts/UnityAdsController.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+using System.Collections;
+using System;
+
+public class UnityAdsController : MonoBehaviour
+{
+	//public Text buttonText;
+	public bool bTestMode=true;
+	public Button button;
+	public Image image;
+	float secondsToShow = 30;	//Tiempo en segundos hasta que vuelva a estar activo el boton
+	bool bShow=false;
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	void Awake()
+	{
+		if(Advertisement.isSupported) {
+			Advertisement.allowPrecache = true;
+			#if UNITY_ANDROID
+				Advertisement.Initialize("35427", bTestMode); //Android
+			#elif
+				Advertisement.Initialize("35428", bTestMode); //iOS
+			#endif
+		}else{
+			Debug.Log("Platform not supported");
+		}
+		button = GetComponent<Button>();
+		image = GetComponent<Image>();
+		bShow=false;
+		button.enabled = false;
+		image.enabled = false;
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//Si al cargar el mapa de los mundos han pasado mas de 3 minutos desde la ultima vez se muestra el cartelito
+	void Start()
+	{
+		DateTime actualTime = DateTime.Now;
+		int secondsElapsed=0;
+		if(PlayerPrefs.HasKey("savedTimeAds")){
+			DateTime savedTime = DateTime.Parse(PlayerPrefs.GetString("savedTimeAds"));
+			secondsElapsed = (int)actualTime.Subtract(savedTime).TotalSeconds;
+			if(secondsElapsed > secondsToShow){
+				bShow=true;
+			}
+		}else{
+			bShow=true;
+		}
+		PlayerPrefs.SetString("savedTimeAds", DateTime.Now.ToString());
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	void Update()
+	{
+		if(Advertisement.isReady() && bShow){
+			button.enabled = true;
+			image.enabled = true;
+		}
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public void OnShowAdButtonPressed()
+	{
+		//Debug.Log("OnShowAdButtonPressed: " + Advertisement.isReady());
+		
+		if(Advertisement.isReady()){
+			Advertisement.Show(null, new ShowOptions { pause = false, resultCallback = ResultCallback } );
+		}
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	void ResultCallback(ShowResult result)
+	{
+		//Debug.Log("-_-_ ResultCallback: " + result.ToString());
+		
+		if(result == ShowResult.Finished){
+			int coins = PlayerPrefs.GetInt("Coins") + 5;
+			CoinsManager.instance.SetCoins(coins);
+			bShow = false;
+			button.enabled = false;
+			image.enabled = false;
+		}
+	}	
+}
+
+
+
+
+
+
