890d9c8d4441c8b320ba7526a565729b40b2138c
aratingadevelopers@gmail.com <aratingadevelopers@gmail.com@712fa71d-62c7-3548-ab67-251bebb63946>
2015-04-30 11:42:26 +0000

    Solucionado un fallo de los videos y otro de los boosters

--- trunk/Assets/Scripts/LanguageManager.cs
+++ trunk/Assets/Scripts/LanguageManager.cs
@@ -96,7 +96,7 @@ public class LanguageManager : MonoBehaviour
 		dict.Add("id_tutorial_paredes", new string[] {"Puedes hacer rebotar las bolas en las paredes", "Use the walls to rebound the bubbles"});
 		dict.Add("id_tutorial_cambia_bolas", new string[] {"Pulsa las flechas para cambiar las bolas", "Switch bubbles by touching the arrows"});
 		dict.Add("id_tutorial_booster_bomba", new string[] {"Usa la bomba para petar varias bolas, incluso obst\u00E1culos", "Use the bomb to destroy several bubbles, including obstacles"});
-		dict.Add("id_tutorial_booster_fuego", new string[] {"Usa la bola fuego para destruir muchas bolas", "Use the fireball to destroy everything in its way"});
+		dict.Add("id_tutorial_booster_fuego", new string[] {"Usa la bola fuego para destruir las bolas en el camino", "Use the fireball to destroy everything in its way"});
 		dict.Add("id_tutorial_booster_multicolor", new string[] {"Usa la bola multicolor para combinar con cualquier color", "Use the Multicolor bubble to combine the match with any color"});
 		dict.Add("id_tutorial_bola_rayo", new string[] {"Toca la Bola Rayo para petar todas las bolas de la l\u00EDnea", "Touch the Line Blast to remove all the bubbles on that line"});
 	}
